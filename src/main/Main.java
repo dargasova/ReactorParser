75207b88c21ffa27db876f9fcc7ee9185f56c42d
dargasova <dianaargasova@gmail.com>
2024-05-27 14:37:26 +0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,61 @@
+package main;
+
+import java.awt.*;
+import java.net.URISyntaxException;
+
+public class Main {
+
+    public static void main(String[] args) throws URISyntaxException {
+        new GUI().setSize(new Dimension(500, 500));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
